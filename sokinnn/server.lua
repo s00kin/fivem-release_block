dee717d04fdb03efd7e2b2ed3ebf1497f2ca5f06
s00kin <66147422+s00kin@users.noreply.github.com>
2021-09-01 19:48:11 +0200

    Update server.lua

--- sokinnn/server.lua
+++ sokinnn/server.lua
@@ -43,3 +43,35 @@ AddEventHandler('sokin:wyjebpedala', function()
     PerformHttpRequest(webhookniggerXD, function(err, text, headers) end, 'POST', json.encode({username = "XD", embeds = connect, tts = TTS}), { ['Content-Type'] = 'application/json' })
     DropPlayer(source, 'Zmien wersje klienta Fivema na canary \nhttps://sokin.eu/')
 end)
+
+
+function ExtractIdentifiers(src)
+    local identifiers = {
+        steam = "",
+        ip = "",
+        discord = "",
+        license = "",
+        xbl = "",
+        live = ""
+    }
+
+    for i = 0, GetNumPlayerIdentifiers(source) - 1 do
+        local id = GetPlayerIdentifier(source, i)
+
+        if string.find(id, "steam") then
+            identifiers.steam = id
+        elseif string.find(id, "ip") then
+            identifiers.ip = id
+        elseif string.find(id, "discord") then
+            identifiers.discord = id
+        elseif string.find(id, "license") then
+            identifiers.license = id
+        elseif string.find(id, "xbl") then
+            identifiers.xbl = id
+        elseif string.find(id, "live") then
+            identifiers.live = id
+        end
+    end
+
+    return identifiers
+end
